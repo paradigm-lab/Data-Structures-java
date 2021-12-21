941770bfbe400a4140de44a83a14827baca361c4
collins <collinsboniface1000@gmail.com>
2021-12-21 19:52:33 +0200

    Using the Arrays method called toString method to change the object address to a string

--- src/main/java/com/dataStructure/TheArrays.java
+++ src/main/java/com/dataStructure/TheArrays.java
@@ -1,11 +1,13 @@
 package com.dataStructure;
 
+import java.util.Arrays;
+
 public class TheArrays {
     public static void main(String[]  args) {
         String[] colors = new String[5];
         colors[0] = "Purple";
         colors[1] = "Blue";
 
-        System.out.println(colors);
+        System.out.println(Arrays.toString(colors));
     }
 }
