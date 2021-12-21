1e1c7988dea08c2e2a4bc4d55bbac99a58073ae9
collins <collinsboniface1000@gmail.com>
2021-12-21 20:11:28 +0200

    Creating the easy way or approach if you now the element in the array and using the for loop to loop through the array from 0 to null and from null to 0

--- src/main/java/com/dataStructure/TheArrays.java
+++ src/main/java/com/dataStructure/TheArrays.java
@@ -17,5 +17,23 @@ public class TheArrays {
 
         colors[2] = "yellow";
         System.out.println(Arrays.toString(colors));
+
+        /*
+        * We are using this approach if we now the element that our array will have
+         */
+        int[] numbers = {100, 200};
+
+        for (int i = 0; i < colors.length; i++) {
+            System.out.println(colors[i]);
+        }
+
+        /*
+        *   Using the reverse approach
+         */
+        System.out.println();
+        System.out.println("Using the Reverse approach");
+        for (int i = colors.length - 1; i >= 0; i--) {
+            System.out.println(colors[i]);
+        }
     }
 }
