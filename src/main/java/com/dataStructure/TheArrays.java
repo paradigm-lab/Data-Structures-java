11d92d6e29a12fd6c31cfc3719bc852bb5a0dcab
collins <collinsboniface1000@gmail.com>
2021-12-21 19:49:32 +0200

    Making the main method and creating the array and the array object and using the index to insert the values into the array using the index

--- src/main/java/com/dataStructure/TheArrays.java
+++ src/main/java/com/dataStructure/TheArrays.java
@@ -2,6 +2,10 @@ package com.dataStructure;
 
 public class TheArrays {
     public static void main(String[]  args) {
-        
+        String[] colors = new String[5];
+        colors[0] = "Purple";
+        colors[1] = "Blue";
+
+        System.out.println(colors);
     }
 }
