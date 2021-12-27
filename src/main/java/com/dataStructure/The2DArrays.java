0a54fbccac8a3e3091f2ef4a27f4d263a793578e
collins <collinsboniface1000@gmail.com>
2021-12-27 16:14:46 +0200

    using the for loop to print out the entire array without using the Arrays method

--- src/main/java/com/dataStructure/The2DArrays.java
+++ src/main/java/com/dataStructure/The2DArrays.java
@@ -11,9 +11,14 @@ public class The2DArrays {
             for (int j = 0; j < 3; j++) {
                  board[i][j] = '-';
             }
-            //System.out.println("\n");
         }
 
-        System.out.println(Arrays.deepToString(board));
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++){
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+        //System.out.println(Arrays.deepToString(board));
     }
 }
