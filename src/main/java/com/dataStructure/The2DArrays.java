93ddb3f1b61c55e1e8b0335cac9b1b3496dd806f
collins <collinsboniface1000@gmail.com>
2021-12-27 16:11:22 +0200

    Creating a tic tac toe game board using the 2 dimenstional arrays

--- src/main/java/com/dataStructure/The2DArrays.java
+++ src/main/java/com/dataStructure/The2DArrays.java
@@ -1,7 +1,19 @@
 package com.dataStructure;
 
+import java.util.Arrays;
+
 public class The2DArrays {
     public static void main(String[] args) {
+        char[][] board = new char[3][3];
 
+        // Assigning values into the 2 dimensional array
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                 board[i][j] = '-';
+            }
+            //System.out.println("\n");
+        }
+
+        System.out.println(Arrays.deepToString(board));
     }
 }
