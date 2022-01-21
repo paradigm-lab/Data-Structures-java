786cdcc74b9151ea261f6bb551a96d5fe3ea8a06
collins <collinsboniface1000@gmail.com>
2022-01-21 22:25:38 +0200

    Created a static method and Created the Queue object of the Person type and add the Person into the Queue

--- src/main/java/com/dataStructure/WorkingWithQueues.java
+++ src/main/java/com/dataStructure/WorkingWithQueues.java
@@ -1,7 +1,23 @@
 package com.dataStructure;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class WorkingWithQueues {
+
+    // Created a static method
+    static record Person(String name, int age) {
+
+    }
+
     public static void main(String[] args) {
 
+        // Created the Queue Object of the type person
+        Queue<Person> supermarket = new LinkedList<>();
+
+        // Using the object to add the person into the class
+        supermarket.add(new Person("Alex", 21));
+        supermarket.add(new Person("Mariam", 18));
+        supermarket.add(new Person("Ali", 60));
     }
 }
