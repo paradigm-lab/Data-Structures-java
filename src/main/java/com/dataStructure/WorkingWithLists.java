be4f742146b2c10998a2084e945c7fc6827635d6
collins <collinsboniface1000@gmail.com>
2022-01-21 21:25:25 +0200

    Using the method to check for the size of the Arraylist and contains method to check for the values that containes in the array list

--- src/main/java/com/dataStructure/WorkingWithLists.java
+++ src/main/java/com/dataStructure/WorkingWithLists.java
@@ -11,7 +11,16 @@ public class WorkingWithLists {
         //Using the add method to add the values in the array list
         colors.add("Blue");
         colors.add("Purple");
+        colors.add("Yellow");
 
+        // Using the size method to check the size of the array
+        System.out.println(colors.size());
+
+        // Using the contains method to check if the string value contains in the array list
+        System.out.println(colors.contains("yellow"));
+        System.out.println(colors.contains("pink"));
+
+        // Printing out the arraylist
         System.out.println(colors);
     }
 }
