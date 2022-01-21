e16dfb80bcf782d0985b8cb19ca7c31126c658c1
collins <collinsboniface1000@gmail.com>
2022-01-21 22:32:09 +0200

    Used the sys out mehtod to see the size change after the peek and poll method

--- src/main/java/com/dataStructure/WorkingWithQueues.java
+++ src/main/java/com/dataStructure/WorkingWithQueues.java
@@ -20,7 +20,14 @@ public class WorkingWithQueues {
         supermarket.add(new Person("Mariam", 18));
         supermarket.add(new Person("Ali", 60));
 
+        // Using the peek method to see the first person in the queue
+        System.out.println(supermarket.size());
         System.out.println(supermarket.peek());
+
+        // Using the poll method to remove the person from the queue
+        System.out.println(supermarket.poll());
+        System.out.println(supermarket.size());
+
 
     }
 }
