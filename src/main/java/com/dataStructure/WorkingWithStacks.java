2f04247666823f0800b81b08a92605a7d4df4730
collins <collinsboniface1000@gmail.com>
2022-01-21 21:56:26 +0200

    Created a new file for working with stacks where by we have created a stack object

---
+++ src/main/java/com/dataStructure/WorkingWithStacks.java
@@ -0,0 +1,13 @@
+package com.dataStructure;
+
+import java.util.Stack;
+
+public class WorkingWithStacks {
+    public static void main(String[] args) {
+        // Creating a stack object
+        Stack<Integer> stack = new Stack<>();
+
+
+
+    }
+}
