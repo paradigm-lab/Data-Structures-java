2e70d325d0fb7642b8acbab36f27e66fc1e90c91
collins <collinsboniface1000@gmail.com>
2022-01-21 21:12:23 +0200

    Created a new file which i will use to Explore more about the List interface and the implementation classes such as Arraylist

---
+++ src/main/java/com/dataStructure/WorkingWithLists.java
@@ -0,0 +1,10 @@
+package com.dataStructure;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkingWithLists {
+    public static void main(String[] args) {
+        List colors = new ArrayList();
+    }
+}
