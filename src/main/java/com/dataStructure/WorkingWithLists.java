c90f4ddf7da0dc16997cfa588bb8b7f6de322c70
collins <collinsboniface1000@gmail.com>
2022-01-21 21:17:15 +0200

    Adding the comment to what is happening in each line and what we are doing is using the Arraylist object and using the add method to add different values into the arraylist and this is possible because we haven't specified the generic type of the class

--- src/main/java/com/dataStructure/WorkingWithLists.java
+++ src/main/java/com/dataStructure/WorkingWithLists.java
@@ -5,6 +5,16 @@ import java.util.List;
 
 public class WorkingWithLists {
     public static void main(String[] args) {
+        // Creating an object for the ArrayList class which is implementing the List interface
         List colors = new ArrayList();
+
+        //Using the add method to add the values in the array list
+        colors.add("Blue");
+        colors.add("Purple");
+        colors.add(1);
+        colors.add(new Object());
+
+
+
     }
 }
