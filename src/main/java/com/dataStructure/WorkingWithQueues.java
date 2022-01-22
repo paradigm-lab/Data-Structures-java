15d551746698084bd2d09f187ef1a4d86a0c463b
collins <collinsboniface1000@gmail.com>
2022-01-22 21:35:54 +0200

    Moved the supermarket object into the queue method and Created a new linkedlist object and use the available method to iterator throught the LinkedList and Using the Iterator object to iterator throught the LinkedList

--- src/main/java/com/dataStructure/WorkingWithQueues.java
+++ src/main/java/com/dataStructure/WorkingWithQueues.java
@@ -1,6 +1,7 @@
 package com.dataStructure;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class WorkingWithQueues {
@@ -12,6 +13,18 @@ public class WorkingWithQueues {
 
     public static void main(String[] args) {
 
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(new Person("Alex", 21));
+        linkedList.add(new Person("Maria", 20));
+
+        // Created a list iterator object which we will use to iterator throughout the LinkedList
+        ListIterator<Person> personListIterator = linkedList.listIterator();
+        while (personListIterator.hasNext()) {
+            System.out.println(personListIterator.next());
+        }
+    }
+
+    private static void queues() {
         // Created the Queue Object of the type person
         Queue<Person> supermarket = new LinkedList<>();
 
@@ -29,6 +42,5 @@ public class WorkingWithQueues {
         System.out.println(supermarket.size());
 
         System.out.println(supermarket.peek());
-
     }
 }
