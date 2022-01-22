864958f8252a90895c02ec2f3d2892a3598b588f
collins <collinsboniface1000@gmail.com>
2022-01-22 21:55:14 +0200

    Created a new static method and created an object for the set interface and the HashSet as the Concrete class of the interface

--- src/main/java/com/dataStructure/WorkingWithSets.java
+++ src/main/java/com/dataStructure/WorkingWithSets.java
@@ -1,4 +1,16 @@
 package com.dataStructure;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class WorkingWithSets {
+
+    record Ball(String color) {
+
+    }
+
+    public static void main(String[] args) {
+        Set<Ball> balls = new HashSet<>();
+
+    }
 }
