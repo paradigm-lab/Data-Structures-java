416259ed850c55d70f247b88bc0d1dc76b5de004
collins <collinsboniface1000@gmail.com>
2022-01-23 22:08:12 +0200

    Created a map objec which takes a key and value

--- src/main/java/com/dataStructure/WorkingWithMaps.java
+++ src/main/java/com/dataStructure/WorkingWithMaps.java
@@ -1,4 +1,25 @@
 package com.dataStructure;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class WorkingWithMaps {
+
+    record Person(String name) {
+
+    }
+
+    public static void main(String[] args) {
+        // Created a map object
+        Map<Integer, Person> map = new HashMap<>();
+
+        // Using the put method to add the key and value in the map
+        map.put(1, new Person("Alex"));
+        map.put(2, new Person("Collins"));
+        map.put(3, new Person("Mariam"));
+
+        // Printing out the map
+        System.out.println(map);
+    }
+
 }
