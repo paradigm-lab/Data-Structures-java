074159cf120c1d41f89ec54a5a14803c72f2df6a
collins <collinsboniface1000@gmail.com>
2022-01-23 11:41:56 +0200

    I overrided the equals and hashCode method in our class so that the set could work properly and toString method to print out the output in a formarted manner

--- src/main/java/com/dataStructure/WorkingWithSets.java
+++ src/main/java/com/dataStructure/WorkingWithSets.java
@@ -1,6 +1,7 @@
 package com.dataStructure;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class WorkingWithSets {
@@ -15,6 +16,26 @@ public class WorkingWithSets {
         // A class constructor
         public Ball(String color) {
             this.color = color;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Ball ball = (Ball) o;
+            return Objects.equals(color, ball.color);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(color);
+        }
+
+        @Override
+        public String toString() {
+            return "Ball{" +
+                    "color='" + color + '\'' +
+                    '}';
         }
     }
 
