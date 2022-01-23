eff2d155b6c012a18d55367b8f631963f80aab50
collins <collinsboniface1000@gmail.com>
2022-01-23 11:39:14 +0200

    Created a static class with the field and the constructor and created an HashSet object of the Ball type and used the add method to add the values into the set and we have got the duplicate in our set and set doesn't count out the duplicate but we get the size as four by also counting out the duplicate this is because They are comparing the values in the memory wether they are pointing to the same object

--- src/main/java/com/dataStructure/WorkingWithSets.java
+++ src/main/java/com/dataStructure/WorkingWithSets.java
@@ -5,15 +5,30 @@ import java.util.Set;
 
 public class WorkingWithSets {
 
-    record Ball(String color) {
+    //record Ball(String color) {}
 
+    // A static class which replaced the record method
+    static class Ball {
+        // Filed in the class
+        String color;
+
+        // A class constructor
+        public Ball(String color) {
+            this.color = color;
+        }
     }
 
     public static void main(String[] args) {
         Set<Ball> balls = new HashSet<>();
         balls.add(new Ball("Blue"));
+        balls.add(new Ball("Blue"));
         balls.add(new Ball("Yellow"));
         balls.add(new Ball("Red"));
+
+        // Printing out the size of the Hashset
+        System.out.println(balls.size());
+
+        // Using the forEach to print out the values in the set
         balls.forEach(System.out::println);
     }
 }
